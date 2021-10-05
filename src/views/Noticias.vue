5796a3ac3c8bab5af3543020e33fcee3603cb8b0
carlose-sg <carlose.sanchez@outlook.com>
2021-10-05 12:24:35 -0500

    Cambios vista Noticias

--- src/views/Noticias.vue
+++ src/views/Noticias.vue
@@ -21,8 +21,12 @@
           <br />
           Te invitamos a incluirla en tus favoritos.
         </p>
-        <br />
         <v-divider></v-divider>
+        <br />
+      </div>
+      <div>
+        <br />
+        <h3>PUBLICACIONES RECIENTES</h3>
         <br />
       </div>
       <v-row dense>
@@ -34,13 +38,13 @@
               class="black--text align-end"
               src="https://www.portafolio.co/files/article_multimedia/uploads/2019/01/28/5c4f12efa7dda.jpeg"
             >
-              <v-card-title>
-                <div>
-                  Abren segundo plazo para aplicar a <br />
-                  subsidios por contratar a jóvenes
-                </div>
-              </v-card-title>
             </v-img>
+            <v-card-text>
+              <h3>
+                Abren segundo plazo para aplicar a subsidios por contratar a
+                jóvenes
+              </h3>
+            </v-card-text>
             <v-card-subtitle class="pb-0">Agosto 31, 2021</v-card-subtitle>
             <v-card-text class="text--primary">
               <div>
@@ -62,6 +66,7 @@
             </v-card-actions>
           </v-card>
         </v-col>
+
         <v-col>
           <v-card hover class="mx-auto" max-width="375">
             <v-img
@@ -70,13 +75,13 @@
               class="white--text align-end"
               src="https://www.portafolio.co/files/article_multimedia/uploads/2021/07/10/60e950565588a.jpeg"
             >
-              <v-card-title>
-                <div>
-                  En julio, mujeres consiguieron más <br />
-                  trabajo que los hombres
-                </div>
-              </v-card-title>
             </v-img>
+            <v-card-text>
+              <h3>
+                En julio, mujeres consiguieron más trabajo que los hombres
+              </h3>
+            </v-card-text>
+
             <v-card-subtitle class="pb-0">Agosto 31, 2021</v-card-subtitle>
             <v-card-text class="text--primary">
               <div>
@@ -97,6 +102,7 @@
             </v-card-actions>
           </v-card>
         </v-col>
+
         <v-col>
           <v-card hover class="mx-auto" max-width="375">
             <v-img
@@ -105,14 +111,15 @@
               class="white--text align-end"
               src="https://www.portafolio.co/files/article_multimedia/uploads/2021/09/02/613170550e90e.jpeg"
             >
-              <v-card-title>
-                <div>
-                  Más de 209.000 personas se <br />
-                  'convirtieron' al teletrabajo durante <br />
-                  2020
-                </div>
-              </v-card-title>
             </v-img>
+
+            <v-card-text>
+              <h3>
+                Más de 209.000 personas se 'convirtieron' al teletrabajo durante
+                2020
+              </h3>
+            </v-card-text>
+
             <v-card-subtitle class="pb-0">Septiembre 02, 2021</v-card-subtitle>
             <v-card-text class="text--primary">
               <div>
@@ -134,8 +141,9 @@
             </v-card-actions>
           </v-card>
         </v-col>
+        <br />
       </v-row>
-      <v-row>
+      <v-row dense>
         <v-col>
           <v-card hover class="mx-auto" max-width="375">
             <v-img
@@ -144,14 +152,15 @@
               class="white--text align-end"
               src="https://www.portafolio.co/files/article_multimedia/uploads/2020/12/29/5feb8ec096adb.jpeg"
             >
-              <v-card-title>
-                <div>
-                  Samsung abre curso para 600 <br />
-                  jóvenes en codificación y <br />
-                  programación
-                </div>
-              </v-card-title>
             </v-img>
+
+            <v-card-text>
+              <h3>
+                Samsung abre curso para 600 jóvenes en codificación y
+                programación
+              </h3>
+            </v-card-text>
+
             <v-card-subtitle class="pb-0">Septiembre 03, 2021</v-card-subtitle>
             <v-card-text class="text--primary">
               <div>
@@ -173,6 +182,220 @@
             </v-card-actions>
           </v-card>
         </v-col>
+
+        <v-col>
+          <v-card hover class="mx-auto" max-width="375">
+            <v-img
+              dark
+              content
+              class="white--text align-end"
+              src="https://www.portafolio.co/files/article_multimedia/uploads/2021/10/03/615a3e25a7bd0.jpeg"
+            >
+            </v-img>
+            <v-card-text>
+              <h3>
+                Claves para una mejor transformación laboral pensando en el
+                futuro
+              </h3>
+            </v-card-text>
+            <v-card-subtitle class="pb-0">Octubre 03, 2021</v-card-subtitle>
+            <v-card-text class="text--primary">
+              <div>
+                El 93 % de los directivos quiere mantener un modelo de trabajo
+                híbrido a 2025.
+              </div>
+            </v-card-text>
+            <v-divider></v-divider>
+            <v-card-actions>
+              <v-btn
+                color="orange"
+                text
+                href="https://www.portafolio.co/economia/empleo/claves-para-transformar-los-espacios-de-trabajo-de-cara-al-futuro-556959"
+                target="blank"
+              >
+                Leer noticia completa
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+
+        <v-col>
+          <v-card hover class="mx-auto" max-width="375">
+            <v-img
+              dark
+              content
+              class="white--text align-end"
+              src="https://www.portafolio.co/files/article_multimedia/uploads/2018/09/24/5ba93c5c125d8.jpeg"
+            >
+            </v-img>
+            <v-card-text>
+              <h3>
+                Las nuevas reglas sobre el contrato por prestación de servicios
+              </h3>
+            </v-card-text>
+            <v-card-subtitle class="pb-0">Octubre 03, 2021</v-card-subtitle>
+            <v-card-text class="text--primary">
+              <div>
+                Esa forma de contratación, en la que existen contratos
+                sucesivos, puede esconder una verdadera relación laboral.
+              </div>
+            </v-card-text>
+            <v-divider></v-divider>
+            <v-card-actions>
+              <v-btn
+                color="orange"
+                text
+                href="https://www.portafolio.co/economia/empleo/contrato-por-prestacion-de-servicios-lo-que-cambia-con-el-fallo-de-la-corte-556789"
+                target="blank"
+              >
+                Leer noticia completa
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+        <br />
+      </v-row>
+
+      <v-row dense>
+        <v-col>
+          <v-card hover class="mx-auto" max-width="375">
+            <v-img
+              dark
+              content
+              class="white--text align-end"
+              src="https://www.portafolio.co/files/article_multimedia/uploads/2016/02/10/56bb54fd2e3df.jpeg"
+            >
+            </v-img>
+
+            <v-card-text>
+              <h3>
+                Colombia, a 424.000 empleos de recuperar el nivel prepandemia
+              </h3>
+            </v-card-text>
+
+            <v-card-subtitle class="pb-0">Septiembre 30, 2021</v-card-subtitle>
+            <v-card-text class="text--primary">
+              <div>
+                En agosto el desempleo se ubicó en 12,3%. Dane destacó que se
+                perciben mejores resultados frente al 2020 y que la contratación
+                femenina ha repuntado.
+              </div>
+            </v-card-text>
+            <v-divider></v-divider>
+            <v-card-actions>
+              <v-btn
+                color="orange"
+                text
+                href="https://www.portafolio.co/economia/empleo/colombia-a-424-000-empleos-de-recuperar-el-nivel-prepandemia-556889"
+                target="blank"
+              >
+                Leer noticia completa
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+
+        <v-col>
+          <v-card hover class="mx-auto" max-width="375">
+            <v-img
+              dark
+              content
+              class="white--text align-end"
+              src="https://www.portafolio.co/files/article_multimedia/uploads/2021/09/30/615650bc42fbd.jpeg"
+            >
+            </v-img>
+
+            <v-card-text>
+              <h3>
+                Fenalco propone contratación por horas y revisión del salario
+                mínimo
+              </h3>
+            </v-card-text>
+
+            <v-card-subtitle class="pb-0">Septiembre 30, 2021</v-card-subtitle>
+            <v-card-text class="text--primary">
+              <div>
+                Jaime Alberto Cabal, presidente del gremio, comentó que es
+                necesaria una reforma laboral.
+              </div>
+            </v-card-text>
+            <v-divider></v-divider>
+            <v-card-actions>
+              <v-btn
+                color="orange"
+                text
+                href="https://www.portafolio.co/economia/empleo/empleo-en-colombia-fenalco-propone-contratacion-por-horas-556884"
+                target="blank"
+              >
+                Leer noticia completa
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+
+        <v-col>
+          <v-card hover class="mx-auto" max-width="375">
+            <v-img
+              dark
+              content
+              class="white--text align-end"
+              src="https://www.portafolio.co/files/article_multimedia/uploads/2020/11/06/5fa5f32001956.jpeg"
+            >
+            </v-img>
+
+            <v-card-text>
+              <h3>Revolución del empleo llegó para quedarse</h3>
+            </v-card-text>
+
+            <v-card-subtitle class="pb-0">Octubre 03, 2021</v-card-subtitle>
+            <v-card-text class="text--primary">
+              <div>
+                Legislaciones y contratos de trabajo cambiarán, pues horarios y
+                presencialidad no serán lo importante, sino los objetivos y el
+                cumplimiento de metas.
+              </div>
+            </v-card-text>
+            <v-divider></v-divider>
+            <v-card-actions>
+              <v-btn
+                color="orange"
+                text
+                href="https://www.portafolio.co/economia/empleo/revolucion-del-empleo-llego-para-quedarse-556714"
+                target="blank"
+              >
+                Leer noticia completa
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+        <br />
+      </v-row>
+
+      <v-row dense>
+        <br />
+        <v-divider></v-divider>
+        <br />
+      </v-row>
+
+      <v-row>
+        <div>
+          <h5>
+            Noticias cortes&iacute;a:
+            <a
+              title="Portafolio"
+              href="https://www.portafolio.co/"
+              target="_blank"
+              >Portafolio</a
+            >
+            un producto de
+            <a
+              title="Casa Editorial El Tiempo"
+              href="https://www.eltiempocasaeditorial.com/"
+              target="_blank"
+              >Casa Editorial El Tiempo</a
+            >
+          </h5>
+        </div>
       </v-row>
     </v-container>
   </v-content>
@@ -200,6 +423,16 @@ h1 {
 }
 
 h3 {
+  text-align: center;
+  color: #f3b340;
+}
+
+h4 {
+  text-align: left;
+  color: #f3b340;
+}
+
+h5 {
   text-align: center;
   color: #f3b340;
 }
